3745e56b13893ed6f6e85dffae6c18db899286eb
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-08-06 11:29:17 +0530

    first1 commit

--- Northwind-Data-workDay1.sql
+++ Northwind-Data-workDay1.sql
@@ -1,3 +1,32 @@
+-- Northwind database
+
+-- view all databases in your system
 SHOW databases;
+
+-- Select & Choose the specific database where you want to work
 USE northwind;
+
+-- view all tables in selected database
 SHOW tables;
+
+-- Output
++---------------------+
+| Tables_in_northwind |
++---------------------+
+| categories          |
+| customers           |
+| dept                |
+| emp                 |
+| employees           |
+| orderdetails        |
+| orders              |
+| persons             |
+| products            |
+| shippers            |
+| suppliers           |
++---------------------+
+11 rows in set (0.02 sec)
+
+-- view all data in  customers table.
+SELECT * FROM customers;
+
